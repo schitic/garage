cc0ae1ab2201b497c15ba5cc362dab25fd9868bc
Stefan-Gabriel Chitic <stefan-gabriel.chitic@cern.ch>
2019-05-27 22:57:13 +0200

    Added actor for local door

--- GarageDeamon/Actors/LocalDoor.py
+++ GarageDeamon/Actors/LocalDoor.py
@@ -5,7 +5,7 @@ import time
 
 class LocalDoor(ActorBase):
 
-    def __init__(self, outputPin=23):
+    def __init__(self, outputPin=24):
         self.commandPin = outputPin
         self._setup()
         super(LocalDoor, self).__init__()
