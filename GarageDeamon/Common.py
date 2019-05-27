592d34fc78ee7339dc44a8767f964e3f0705277c
Stefan-Gabriel Chitic <stefan-gabriel.chitic@cern.ch>
2019-05-27 22:05:37 +0200

    Fix bug in slqlite

--- GarageDeamon/Common.py
+++ GarageDeamon/Common.py
@@ -24,10 +24,10 @@ class SensorBase(object):
 
     def _merge_state(self):
         db_state = Utils.get_state(self.sensor_name)
-        print(db_state, self.current_state)
         if self.current_state is None:
             self.current_state = db_state
         elif self.current_state != db_state:
+            print(db_state, self.current_state)
             self.log.write("Changed Value: from %s to %s" %
                            (db_state, self.current_state),
                            "DBMERGE", component_id=self.sensor_name)
