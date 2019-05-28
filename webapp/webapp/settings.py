cabb786093beb3d86386d53d86e0dcddb69246b0
Stefan-Gabriel Chitic <stefan-gabriel.chitic@cern.ch>
2019-05-28 20:03:36 +0200

    Added fix db path

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '=v=)e%x^2-5e_g+e=eoryb@0tfj!z_ye850m(j(ku$57#&wqh8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
