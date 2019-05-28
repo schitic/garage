87c0709cf078552bcd526e2d619067b468d0b096
Stefan-Gabriel Chitic <stefan-gabriel.chitic@cern.ch>
2019-05-28 22:54:58 +0200

    Logic for mag3110

--- GarageDeamon/Sensors/CarPresanceLeft.py
+++ GarageDeamon/Sensors/CarPresanceLeft.py
@@ -2,6 +2,8 @@ from GarageDeamon.Common import SensorBase
 import RPi.GPIO as GPIO
 import threading
 import time
+import smbus
+
 
 class CarSensorLeft(SensorBase):
 
@@ -23,9 +25,37 @@ class CarSensorLeft(SensorBase):
         return html
 
     def _listener(self):
+        bus3 = smbus.SMBus(3)
+        delay = 0.5
+
+        # MAG3110 I2C address 0x0E
+        # Select Control register, 0x10(16)
+        bus3.write_byte_data(0x0E, 0x10, 0x01)
+
         while self._is_running:
-            print "Worker"
-            time.sleep(1)
+            time.sleep(delay)
+
+            # MAG3110 I2C address 0x0E
+            # Read data back from 0x01(1), 6 bytes
+            data = bus3.read_i2c_block_data(0x0E, 0x01, 6)
+
+            # Convert the data
+            xMag = data[0] * 256 + data[1]
+            if xMag > 32767:
+                xMag -= 65536
+
+            yMag = data[2] * 256 + data[3]
+            if yMag > 32767:
+                yMag -= 65536
+
+            zMag = data[4] * 256 + data[5]
+            if zMag > 32767:
+                zMag -= 65536
+
+            # Output data
+            print "X-Axis : %d" % xMag
+            print "Y-Axis : %d" % yMag
+            print "Z-Axis : %d" % zMag
 
     def run(self):
         self._setup()
